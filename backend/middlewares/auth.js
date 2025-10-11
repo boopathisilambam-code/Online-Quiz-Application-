bba8ec553e3e0fb0fb7885a241e5aa177c41b103
boopathisilambam-code <boopathisilambam@gmail.com>
2025-10-11 19:59:20 +0530

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,16 +1,12 @@
-// middleware/auth.js
 const jwt = require('jsonwebtoken');
 
-module.exports = function(req, res, next) {
-  // Get token from header
+module.exports = function (req, res, next) {
   const token = req.header('x-auth-token');
 
-  // Check if no token
   if (!token) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
-  // Verify token
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.user;
