b0b297139b5d7a3b26513db3a9cb90b4a77e4a64
boopathisilambam-code <boopathisilambam@gmail.com>
2025-10-11 21:35:33 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -45,9 +45,8 @@ export const AuthProvider = ({ children }) => {
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ email, password }),
-        }
-      );
+         body: JSON.stringify({ name, email, password, role }),
+});
 
       const text = await res.text();
       let data = {};
